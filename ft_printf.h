6a62f0c0aeedeb4bb69fc1e34339bb2a06d8b048
rdragan <rdragan@student.42heilbronn.de>
2022-12-05 20:07:20 +0100

    added printf.h

---
+++ ft_printf.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rdragan <rdragan@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/05 19:13:07 by rdragan           #+#    #+#             */
+/*   Updated: 2022/12/05 19:50:22 by rdragan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdlib.h>
+# include <stdarg.h>
+# include <unistd.h>
+
+int	ft_printf(const char *text, ...);
+int	ft_putchar(char c);
+int	ft_putstr(char *str);
+
+#endif
